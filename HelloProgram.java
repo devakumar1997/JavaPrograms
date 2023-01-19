c8e2e58ebcd99deebcf84bcf842ced6791332395
devakumar1997 <baireddydevakumarreddy@gmail.com>
2023-01-19 15:32:16 +0530

    added hello program

---
+++ HelloProgram.java
@@ -0,0 +1,20 @@
+package com.preincrement;
+
+public class HelloProgram {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
