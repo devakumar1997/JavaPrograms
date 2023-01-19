b5ff8e5b5b0501ef8c0515c8f7726b560b2f36ea
devakumar1997 <baireddydevakumarreddy@gmail.com>
2023-01-19 15:28:02 +0530

    added sampleexample1

---
+++ SampleExample.java
@@ -0,0 +1,19 @@
+package com.preincrement;
+
+public class SampleExample {
+
+	public static void main(String[] args) {
+
+		int a= 1 ;
+		int b=2;
+		int sum = a + b ;
+		System.out.println("sum of a and b is :"  + sum);
+		
+		
+		
+		
+		
+		
+	}
+
+}
